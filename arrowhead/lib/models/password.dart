639278d198a5e90a6a116644e581a53c330373e1
GustavoLR548 <gustavolr035@gmail.com>
2022-04-28 20:10:13 -0300

    hfix: fixed the use of this.

--- arrowhead/lib/models/password.dart
+++ arrowhead/lib/models/password.dart
@@ -9,10 +9,10 @@ class Password {
   const Password(this._id, this._password, this._name, this._description,
       this._url, this._login);
 
-  String get id => this._id;
-  String get password => this._password;
-  String get name => this._name;
-  String get description => this._description;
-  String get url => this._url;
-  String get login => this._login;
+  String get id => _id;
+  String get password => _password;
+  String get name => _name;
+  String get description => _description;
+  String get url => _url;
+  String get login => _login;
 }
